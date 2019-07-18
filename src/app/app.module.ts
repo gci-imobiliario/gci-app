e17228ffc26f1446a552e90a7924c223d5ef275d
victor <felipe.beleza29@gmail.com>
2019-07-17 21:32:23 -0400

    organização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './core/app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './layout/public/login/login.component';
+
+import { CustomMaterialModule } from './core/material.module';
+import { MatCommonModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CustomMaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
