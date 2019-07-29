f48ed8367fe907ebda0332677ed79365f07b1110
victor <felipe.beleza29@gmail.com>
2019-07-29 18:36:49 -0400

    configurações de rota

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppRoutingModule } from '../app/core/app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from '../app/layout/public/login/login.component';
+import { SidenavComponent } from './layout/layouts/sidenav/sidenav.component';
+import { DashBoardComponent } from './layout/security/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    SidenavComponent,
+    DashBoardComponent
   ],
   imports: [
     BrowserModule,
--- src/app/core/app-routing.module.ts
+++ src/app/core/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '../layout/public/login/login.component';
+import { SidenavComponent } from '../layout/layouts/sidenav/sidenav.component';
+import { DashBoardComponent } from '../layout/security/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: LoginComponent
+    component: LoginComponent,
   },
   {
     path: 'line',
-    component: LoginComponent
+    component: SidenavComponent,
+    children: [
+      { path: 'dashboard', component: DashBoardComponent }
+    ]
   }
 ];
 
